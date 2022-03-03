d17645c10dd0ec94bfe4b39a2f9092e556d8a5d8
prathish-code <88712212+Prathish-codes@users.noreply.github.com>
2022-03-03 13:09:16 +0530

    working on Js part

---
+++ index.js
@@ -0,0 +1,7 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven  =document.querySelector("#cash-given");
+const checkButton =document .querySelector("#check-button");
+
+console.log(cashGiven.value);
+
+
